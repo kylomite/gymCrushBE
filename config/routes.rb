6ae71b5ad43bd51df04d48fead72f36597debc92
kylomite <kdelaney99@gmail.com>
2025-03-31 10:24:42 -0400

    Revert "add scaffold for user_roles"
    This reverts commit 0f5477333de197820662515f95256a62f08f5755.
    
    Revert User Roles scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user_roles
   resources :daily_macros
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
