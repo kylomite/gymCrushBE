0f5477333de197820662515f95256a62f08f5755
kylomite <kdelaney99@gmail.com>
2025-03-31 10:18:35 -0400

    add scaffold for user_roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :user_roles
   resources :daily_macros
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
